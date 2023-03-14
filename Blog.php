7df9385e769021f18d8f47852044fb6489bb3488
sanader <sanaderali007@gmail.com>
2023-03-14 13:15:30 +0500

    new pages add

--- Blog.php
+++ Blog.php
@@ -1,5 +1,145 @@
 <?php include ('header.php'); ?>
 
+<!-- Start Page Banner Area -->
+<div class="page-banner-area jarallax" data-jarallax='{"speed": 0.3}'>
+            <div class="container">
+                <div class="page-banner-content" data-aos="fade-right" data-aos-delay="50" data-aos-duration="500" data-aos-once="true">
+                    <h2>Our Blog</h2>
 
+                    <ul>
+                        <li>
+                            <a href="/Blog.php">Home</a>
+                        </li>
+                        <li>Blog Style Two</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Page Banner Area -->
+        
+        <!-- Start Blog Area -->
+        <div class="blog-area pt-100 pb-100">
+            <div class="container">
+                <div class="section-title">
+                    <span>ARTICLE</span>
+                    <h2>Read Our Blog To Get All Recent Tech <b>News</b> <span class="overlay"></span></h2>
+                </div>
+                
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-7.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">How Technology Dominate In The new World In 2022</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-8.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">Top 10 Most Famous Technology Trend In 2022</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-9.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">Open Source Job Report Show More Openings Fewer</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-10.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">Tech Products That Makes Its Easier To Stay At Home</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-11.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">Necessity May Give Us Your Best Virtual Court System</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-blog-card">
+                            <div class="blog-image">
+                                <a href="single-blog-1.html"><img src="assets/images/blog/blog-12.jpg" alt="image"></a>
+
+                                <div class="date">9th July, 2022</div>
+                            </div>
+                            <div class="blog-content">
+                                <h3>
+                                    <a href="single-blog-1.html">How Technology Helps In The Education System In The World</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eir m od tempor invidunt ut labore.</p>
+                                <a href="single-blog-1.html" class="blog-btn">View More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-12 col-md-12">
+                        <div class="pagination-area">
+                            <a href="#" class="prev page-numbers"><i class="ri-arrow-left-s-line"></i></a>
+                            <span class="page-numbers current" aria-current="page">1</span>
+                            <a href="#" class="page-numbers">2</a>
+                            <a href="#" class="page-numbers">3</a>
+                            <a href="#" class="next page-numbers"><i class="ri-arrow-right-s-line"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Blog Area -->
 
 <?php include ('footer.php'); ?>
