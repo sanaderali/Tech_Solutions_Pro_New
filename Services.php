9ff3530db482d1e418613af6765c09211e054839
sanader <sanaderali007@gmail.com>
2023-03-14 13:14:27 +0500

    all pages file

--- Services.php
+++ Services.php
@@ -2,7 +2,325 @@
 
 
 
+<div class="page-banner-area bg-2 jarallax" data-jarallax='{"speed": 0.3}'>
+            <div class="container">
+                <div class="page-banner-content" data-aos="fade-right" data-aos-delay="50" data-aos-duration="500" data-aos-once="true">
+                    <h2>Services Style One</h2>
 
+                    <ul>
+                        <li>
+                            <a href="index.html">Home</a>
+                        </li>
+                        <li>Services Style One</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!-- End Page Banner Area -->
+
+        <!-- Start Choose Area -->
+        <div class="choose-area pt-100 pb-75">
+            <div class="container">
+                <div class="section-title">
+                    <span>KEY FEATURES</span>
+                    <h2>We Help You To <b>Increase</b> Your Sale Through Solutions <span class="overlay"></span></h2>
+                    <p>We are leading technology solutions providing company all over the world doing over 40 years lorem ipsum dolor sit amet.</p>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="single-choose-card" data-aos="fade-up" data-aos-delay="50" data-aos-duration="500" data-aos-once="true">
+                            <div class="choose-image" data-tilt>
+                                <a href="services-details.html"><img src="assets/images/choose/choose-1.png" alt="image"></a>
+                            </div>
+                            <div class="choose-content">
+                                <h3>
+                                    <a href="services-details.html">Mobile Apps</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eirm od tempor invidunt ut labore.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="single-choose-card" data-aos="fade-up" data-aos-delay="60" data-aos-duration="600" data-aos-once="true">
+                            <div class="choose-image" data-tilt>
+                                <a href="services-details.html"><img src="assets/images/choose/choose-2.png" alt="image"></a>
+                            </div>
+                            <div class="choose-content">
+                                <h3>
+                                    <a href="services-details.html">Branding Strategy</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eirm od tempor invidunt ut labore.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="single-choose-card" data-aos="fade-up" data-aos-delay="70" data-aos-duration="700" data-aos-once="true">
+                            <div class="choose-image" data-tilt>
+                                <a href="services-details.html"><img src="assets/images/choose/choose-3.png" alt="image"></a>
+                            </div>
+                            <div class="choose-content">
+                                <h3>
+                                    <a href="services-details.html">Graphic Design</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eirm od tempor invidunt ut labore.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="single-choose-card" data-aos="fade-up" data-aos-delay="80" data-aos-duration="800" data-aos-once="true">
+                            <div class="choose-image" data-tilt>
+                                <a href="services-details.html"><img src="assets/images/choose/choose-4.png" alt="image"></a>
+                            </div>
+                            <div class="choose-content">
+                                <h3>
+                                    <a href="services-details.html">Search Optimization</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet conset sadipscing elitr sed diam nonumy eirm od tempor invidunt ut labore.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="choose-shape-1">
+                <img src="assets/images/choose/choose-shape.png" alt="image">
+            </div>
+            <div class="choose-shape-2">
+                <img src="assets/images/choose/choose-shape-2.png" alt="image">
+            </div>
+        </div>
+        <!-- End Choose Area -->
+        
+        <!-- Start Services Area -->
+        <div class="services-area-style-two ptb-100">
+            <div class="container-fluid">
+                <div class="section-title">
+                    <span>SERVICES</span>
+                    <h2>We Provide the Best Quality <b>Services</b> <span class="overlay"></span></h2>
+                    <p>We are technology solutions providing company all over the world doing over 40 years. lorem ipsum dolor sit amet.</p>
+                </div>
+                
+                <div class="services-slides-two owl-carousel owl-theme">
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-1.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">Database Administrator</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-2.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">Software Development</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-3.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">App Development</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-4.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">Web Development</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-5.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">Analytic Solutions</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-5.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="services-details.html">Product Design</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+
+                    <div class="services-item">
+                        <div class="services-image">
+                            <a href="services-details.html"><img src="assets/images/services/services-1.jpg" alt="image"></a>
+                        </div>
+                        <div class="services-content">
+                            <h3>
+                                <a href="#">Cloud & DevOps</a>
+                            </h3>
+                            <p>Lorem ipsum dolor sit amet con setetur sadipscing elitr sed…</p>
+                            <a href="services-details.html" class="services-btn">View More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="services-shape-1">
+                <img src="assets/images/services/services-shape-1.png" alt="image">
+            </div>
+            <div class="services-shape-2">
+                <img src="assets/images/services/services-shape-2.png" alt="image">
+            </div>
+        </div>
+        <!-- End Services Area -->
+
+        <!-- Start Talk Area -->
+        <div class="talk-area ptb-100">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="talk-image" data-tilt>
+                            <img src="assets/images/talk/talk-2.png" alt="image">
+
+                            <div class="talk-circle">
+                                <img src="assets/images/talk/talk-circle.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-12">
+                        <div class="talk-content margin-zero">
+                            <span>LET'S TALK</span>
+                            <h3>We Would Like To Hear From You Any Question <span class="overlay"></span></h3>
+                            <p>Lorem ipsum dolor sit amet consetetur sadipscing elitre sed diam non umy eirmod tempor invidunt ut labore.</p>
+
+                            <form id="contactFormTwo">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="name" class="form-control" required data-error="Please enter your name" placeholder="Your name">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control" required data-error="Please enter your email" placeholder="Your email address">
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12 col-md-12">
+                                        <div class="form-group">
+                                            <textarea name="message" class="form-control" cols="30" rows="6" required data-error="Please enter your message" placeholder="Write your message..."></textarea>
+                                            <div class="help-block with-errors"></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-lg-12 col-md-12">
+                                        <button type="submit" class="default-btn">Send Message<span></span></button>
+                                        <div id="msgSubmitTwo" class="h3 text-center hidden"></div>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Talk Area -->
+
+        <!-- Start Overview Area -->
+        <div class="overview-area pt-100 pb-75">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="overview-card">
+                            <h3>Call Us</h3>
+                            <span>
+                                <a href="tel:9901234567">+990-123-4567</a>
+                            </span>
+
+                            <div class="overview-shape">
+                                <img src="assets/images/overview/overview-shape.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="overview-card">
+                            <h3>Email Us</h3>
+                            <span>
+                                <a href="https://templates.envytheme.com/cdn-cgi/l/email-protection#b6dedfd5d9ccd3f6d1dbd7dfda98d5d9db"><span class="__cf_email__" data-cfemail="6a02030905100f2a0d070b030644090507">[email&#160;protected]</span></a>
+                            </span>
+
+                            <div class="overview-shape">
+                                <img src="assets/images/overview/overview-shape.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="overview-card">
+                            <h3>Tech Support</h3>
+                            <span>
+                                <a href="tel:15143125678">+1 (514) 312-5678</a>
+                            </span>
+
+                            <div class="overview-shape">
+                                <img src="assets/images/overview/overview-shape.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="overview-card">
+                            <h3>Visit Us</h3>
+                            <span>413 North Las Vegas, NV 89032</span>
+
+                            <div class="overview-shape">
+                                <img src="assets/images/overview/overview-shape.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Overview Area -->
 
 
 <?php include ('footer.php'); ?>
--- header.php
+++ header.php
@@ -42,6 +42,56 @@
         </div>
         <!-- End Preloader Area -->
 
+         <!-- Start Topbar Area -->
+         <div class="topbar-area">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-lg-9 col-md-9">
+                        <ul class="topbar-information">
+                            <li>
+                                <i class="ri-phone-line"></i>
+                                <span>Phone: <a href="tel:9901234567">+990-123-4567</a></span>
+                            </li>
+                            <li>
+                                <i class="ri-mail-line"></i>
+                                <span>Mail: <a href="https://templates.envytheme.com/cdn-cgi/l/email-protection#c9a1a0aaa6b3ac89aea4a8a0a5e7aaa6a4"><span class="__cf_email__" data-cfemail="e78f8e84889d82a7808a868e8bc984888a">[email&#160;protected]</span></a></span>
+                            </li>
+                            <li>
+                                <i class="ri-map-pin-line"></i>
+                                <span>Address:</span> 413 North Las Vegas, NV 89032
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-lg-3 col-md-3">
+                        <ul class="topbar-action">
+                            <li class="dropdown language-option">
+                                <button class="dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="ri-global-line"></i>
+                                    <span class="lang-name"></span>
+                                </button>
+                                <div class="dropdown-menu language-dropdown-menu">
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/images/uk.png" alt="flag">
+                                        ENG
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/images/china.png" alt="flag">
+                                        简体中文
+                                    </a>
+                                    <a class="dropdown-item" href="#">
+                                        <img src="assets/images/uae.png" alt="flag">
+                                        العربيّة
+                                    </a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Topbar Area -->
+
         <!-- Start Header Area -->
         <header class="main-header-area">
             <!-- Start Navbar Area -->
@@ -216,8 +266,8 @@
                                     </li>
 
                                     <li class="nav-item">
-                                        <a href="/Projects.php" class="nav-link">
-                                            Projects 
+                                        <a href="/Portfolio.php" class="nav-link">
+                                            Portfolio
                                             <!-- <i class="ri-arrow-down-s-line"></i> -->
                                         </a>
                                         <!-- <ul class="dropdown-menu">
